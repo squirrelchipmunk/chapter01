6ee0e886e097704c4c062dbb8d3d81a5bf2e61d4
이원준 <ggoong8@gmail.com>
2021-11-17 12:16:17 +0900

    while, for

---
+++ src/com/javaex/ex04/Ex06.java
@@ -0,0 +1,15 @@
+package com.javaex.ex04;
+
+public class Ex06 {
+
+	public static void main(String[] args) {
+
+		for(int i=2;i<=9;i++) {
+			for(int j=1;j<=9;j++) {
+				System.out.println(i+" * "+j+" = "+(i*j));
+			}
+			System.out.println();	
+		}
+	}
+
+}
